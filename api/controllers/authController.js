aa76f4998bac5173cf62bbb97a7e1b17dd447867
Prekshya12 <prekshyabhatta@gmail.com>
2023-09-30 22:43:35 +0530

    Fixed Security Bug

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -79,7 +79,7 @@ exports.protect = catchAsync(async (req, res, next)=>{
 
     // 2) Verification of token
     const decoded= await promisify(jwt.verify)(token, process.env.JWT_SECRET)  //it is by default not a promise function but from the starting we are working with promises therefore we will make this fun as a promise by using a node module so that we can use async await functionality here as well
-    console.log(decoded);
+    
 
     // 3) check if user still exists
     const currentUser = await User.findById(decoded.id);
